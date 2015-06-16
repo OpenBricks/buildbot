10571b0ec12f665b1f19c3368f4c571b32f6b2d1
Thomas Genty <tomlohave@gmail.com>
2015-06-16 07:42:07 +0200

    check for /tmp/openbricks symlink

--- buildbot.sh
+++ buildbot.sh
@@ -122,6 +122,9 @@ find $LOGS/$REPONAME/*.log* -mtime +10 -delete
 # build configs
 conf_enable="a10-cubieboard armada5xx-cubox bcm2708-raspberrypi bcm2709-raspberrypi2 i386-generic imx6-cuboxi imx6-utilite x86_64-generic"
 for config_f in $conf_enable ; do
+  if [ -L /tmp/openbricks ]  ; then
+    rm /tmp/openbricks
+  fi
   conffile="$REPO/config/defconfigs/geexbox-xbmc-$config_f.conf"
   cd $BUILD
   NAME=`basename $conffile .conf`
