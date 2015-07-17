25149a54047df24c4275de5e977591459c55b836
Rudi <r.ihle@s-t.de>
2015-07-17 18:49:42 +0200

    buildbot.sh: Restructure, prepare for kodi, use xz compression for logs

--- buildbot.sh
+++ buildbot.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-BASE=/home/geexbox/bot/buildbot
 REPONAME=openbricks
 REPOURL="https://github.com/OpenBricks/openbricks.git"
+REPOOWNERS="tomlohave@gmail.com nicknickolaev@gmail.com r.ihle@s-t.de"
+
+ACTIVE_CONFIGS=" \
+  geexbox-xbmc-a10-cubieboard \
+  geexbox-xbmc-armada5xx-cubox \
+  geexbox-xbmc-bcm2708-raspberrypi \
+  geexbox-xbmc-bcm2709-raspberrypi2 \
+  geexbox-xbmc-i386-generic \
+  geexbox-xbmc-imx6-cuboxi \
+  geexbox-xbmc-imx6-utilite \
+  geexbox-xbmc-x86_64-generic \
+"
+
+BASE=$HOME/bot/buildbot
 REPO=$BASE/src/$REPONAME
 SOURCES=$BASE/src/sources
 BUILD=$BASE/build
@@ -12,23 +25,23 @@ LOGS=$BASE/logs
 LOGFILE=$BASE/logs/$REPONAME.log
 STAMPSGET=$BASE/src/.stamps
 
-if [ -r $STAMPS/$REPONAME/rev ]; then
-  OLDREV=`cat $STAMPS/$REPONAME/rev`
-fi
 DATE=`date +%Y%m%d`
 
-log()
-{
+log() {
   NOW=`date "+%b %d %T"`
   echo "$NOW [$$] $1" >> $LOGFILE
 }
 
-mailfail()
-{
+compress() {
+  #lbzip2 -9 $1
+  xz -zfe $1
+}
+
+mailfail() {
 cat > /tmp/mesg.txt <<EOF
 Hi,
 
-I was trying to build '$NAME', but something went wrong. The build
+I was trying to build '$CONFNAME', but something went wrong. The build
 failed at '$1' stage,
 ----------
 
@@ -38,47 +51,96 @@ Attached, the log messages
 buildbot
 EOF
 
-mpack -s "[buildbot] $NAME failed to build" -d /tmp/mesg.txt $BUILDLOG.bz2 tomlohave@gmail.com nicknickolaev@gmail.com r.ihle@s-t.de
-
+  mpack -s "[buildbot] $CONFNAME failed to build" -d /tmp/mesg.txt $BUILDLOG.?z* $REPOOWNERS
 }
 
 create_img () {
-  cd $1
-  cd `ls .`
-  if [ -f make-sdcard ] ; then
+  cd $1/*
+  if [ -f make-sdcard ]; then
     a=`grep DEFAULT_TARGET= make-sdcard | cut -d= -f2 | cut -d\" -f2`
     b=`ls *.xz`
     c=`echo $b | sed -e 's/.tar.xz//'`.img
     sudo ./make-sdcard $c $b $a
   fi
+  cd -
 }
 
 clean_old_data () {
   # keep only 3 builds
-    cd $1
-    ls -dt ./* | tail -n +4 | xargs rm -rf || true
+  ls -dt $1/* | tail -n +4 | xargs rm -rf || true
 }
 
-mkdir -p $BUILD $SOURCES $SNAPSHOTS $SNAPSHOTSD $STAMPS/$REPONAME $LOGS $BASE/src/.stamps
+prepare_to_build() {
+  CONFNAME=$1
+  CONFFILE=$2
+
+  # remove symlink
+  [ -L /tmp/openbricks ] && rm /tmp/openbricks
+
+  mkdir -p $LOGS/$REPONAME
+  BUILDLOG="$LOGS/$REPONAME/$CONFNAME.$DATE.log"
+
+  log "Building $CONFNAME"
+  if [ ! -d "$CONFNAME" ]; then
+    log "Cloning $CONFNAME"
+    rm -f $STAMPS/$CONFNAME
+    git clone $REPO $CONFNAME > $BUILDLOG
+    
+    # set links to source packages and download timestamps
+    if [ -n "$CONFFILE" ]; then
+      ln -s $SOURCES $CONFNAME/sources
+      ln -s $STAMPSGET $CONFNAME/.stamps
+    fi
+  fi
+  
+  if [ -z "$CONFFILE" ] || \
+     [ ! -e "$STAMPS/$REPONAME/$CONFNAME" ] || \
+     [ "$CONFFILE" -nt "$STAMPS/$REPONAME/$CONFNAME" ]; then  
+    rm -f "$BUILDLOG"
+    rm -f "$STAMPS/$REPONAME/$CONFNAME"
+
+    (cd $CONFNAME; git pull -u >> $BUILDLOG 2>&1)
+    touch $CONFNAME/NEED_REBUILD
+  else
+    log "Build $CONFNAME is up to date"
+    rm -f $CONFNAME/NEED_REBUILD
+  fi
+}
+
+
+# Create directories
 log "Starting"
+mkdir -p $BUILD $SOURCES $SNAPSHOTS $SNAPSHOTSD $STAMPS/$REPONAME $LOGS $BASE/src/.stamps
+
+# Check for re-entry
 if [ -r $STAMPS/lock ]; then
   log "Another buildbot instance (`cat $STAMPS/lock`) is running, aborting."
   exit 1
-else
-  /bin/echo -n $$ > $STAMPS/lock
-  rm -Rf $BUILD/*
 fi
+
+/bin/echo -n $$ > $STAMPS/lock
+rm -rf $BUILD/*
+
+# delete old logs
+find $LOGS/$REPONAME/*.log* -mtime +10 -delete
+
+# Refresh repo
 if [ ! -d $REPO ]; then
-  log "Cloning repo"
+  log "Cloning repo $REPONAME"
   git clone $REPOURL $REPO > /dev/null 2>&1
 else
-  log "Pulling repo"
+  log "Pulling repo $REPONAME"
   (cd $REPO; git pull -u > /dev/null 2>&1)
 fi
 
+# Check for revision change
 cd $REPO
 REV=`git log -1 --pretty="%h"`
 cd ..
+
+if [ -r $STAMPS/$REPONAME/rev ]; then
+  OLDREV=`cat $STAMPS/$REPONAME/rev`
+fi
 
 if [ "$REV" = "$OLDREV" ]; then
   log "Nothing new"
@@ -88,131 +150,118 @@ else
   echo $REV > $STAMPS/$REPONAME/rev
 fi
 
-# build docs
+
+# build documentation
 cd $BUILD
-NAME=docs
-mkdir -p $LOGS/$REPONAME
-BUILDLOG="$LOGS/$REPONAME/$NAME.$DATE.log"
-log "Building $NAME"
-if [ ! -d "$NAME" ]; then
-  log "Cloning $NAME"
-  rm -f $STAMPS/$NAME
-  git clone $REPO $NAME > $BUILDLOG
-fi
-rm -f "$BUILDLOG"
-cd $NAME
-#hg update -C >> $BUILDLOG 2>&1
-git pull -u >> $BUILDLOG 2>&1
-cd DOCS
-make clean >> $BUILDLOG 2>&1
-if [ $? -ne 0 ]; then
-  log "$NAME clean failed"
-else
-  make >> $BUILDLOG 2>&1
-  if [ $? -eq 0 ]; then
-    log "$NAME build successful"
-    mkdir -p "$SNAPSHOTS/$REPONAME/$NAME/$DATE"
-    cp -PR *.html *.pdf images "$SNAPSHOTS/$REPONAME/$NAME/$DATE/"
-    cp -P $BASE/src/docs/*.png "$SNAPSHOTS/$REPONAME/$NAME/$DATE/images/"
-    rm -f $SNAPSHOTS/$REPONAME/$NAME/latest
-    ln -sf $DATE "$SNAPSHOTS/$REPONAME/$NAME/latest"
-  else
-    log "$NAME build failed"
-  fi
-fi
+prepare_to_build docs ""
 
-#find $SNAPSHOTS/openbricks/geexbox-xbmc-*/* -mtime +10 -delete
-#find $SNAPSHOTS/data/openbricks/geexbox-xbmc-*/* -mtime +10 -delete
-find $LOGS/$REPONAME/*.log* -mtime +10 -delete
+if [ -e $CONFNAME/NEED_REBUILD ]; then
+  cd $CONFNAME/DOCS
 
-# build configs
-conf_enable="a10-cubieboard armada5xx-cubox bcm2708-raspberrypi bcm2709-raspberrypi2 i386-generic imx6-cuboxi imx6-utilite x86_64-generic"
-for config_f in $conf_enable ; do
-  if [ -L /tmp/openbricks ]  ; then
-    rm /tmp/openbricks
-  fi
-  conffile="$REPO/config/defconfigs/geexbox-xbmc-$config_f.conf"
-  cd $BUILD
-  NAME=`basename $conffile .conf`
-  mkdir -p $LOGS/$REPONAME
-  BUILDLOG="$LOGS/$REPONAME/$NAME.$DATE.log"
-  log "Building $NAME"
-  if [ ! -d "$NAME" ]; then
-    log "Cloning $NAME"
-    rm -f $STAMPS/$NAME
-    git clone $REPO $NAME > $BUILDLOG 
-    ln -s $SOURCES $NAME/sources
-    ln -s $STAMPSGET $NAME/.stamps
-  fi
-
-  if [ "$STAMPS/$REPONAME/$NAME" -nt $conffile ]; then
-    log "Build $NAME is up to date"
-    continue
-  fi
-  rm -f "$BUILDLOG"
-  rm -f "$STAMPS/$REPONAME/$NAME"
-  cd $NAME
-#  hg update -C >> $BUILDLOG 2>&1
-  git pull -u >> $BUILDLOG 2>&1
-  ./scripts/kconfiginit >> $BUILDLOG 2>&1
-  if grep -q 'CONFIG_OPT_TARGET_FLAT=y' config/defconfigs/$NAME.conf; then
-    sed \
-      -e 's:CONFIG_OPT_TARGET_FLAT=y:# CONFIG_OPT_TARGET_FLAT is not set:' \
-      -e 's:# CONFIG_OPT_TARGET_TARBALL is not set:CONFIG_OPT_TARGET_TARBALL=y:' \
-      -e 's:CONFIG_OPT_CONCURRENCY_MAKE_LEVEL=8:CONFIG_OPT_CONCURRENCY_MAKE_LEVEL=8:' \
-      -e 's:CONFIG_OPT_CONCURRENCY_MAKE_LEVEL=9:CONFIG_OPT_CONCURRENCY_MAKE_LEVEL=8:'\
-      < config/defconfigs/$NAME.conf \
-      > `ls -d build/build.host/kconfig-frontends-*`/.config
-  else
-    cp -P config/defconfigs/$NAME.conf `ls -d build/build.host/kconfig-frontends-*`/.config
-  fi
-  make silentoldconfig >> $BUILDLOG 2>&1 || true
-  make quickclean >> $BUILDLOG 2>&1
+  log "Cleaning $CONFNAME"
+  make clean >> $BUILDLOG 2>&1
   if [ $? -ne 0 ]; then
-    log "$NAME quickclean failed"
-    mailfail clean
-    rm -f "$STAMPS/$REPONAME/$NAME"
-    continue
-  fi
-  rm -rf binaries
-  log "Making $NAME"
-  local_rev=`git log -1 --pretty="%h"`
-  make >> $BUILDLOG 2>&1
-  if [ $? -eq 0 ]; then
-    echo "Build successful : local revision is $local_rev" >> $BUILDLOG 2>&1
-    log "$NAME build successful"
-    echo $DATE > "$STAMPS/$REPONAME/$NAME"
-    mkdir -p "$SNAPSHOTSD/$REPONAME/$NAME/$DATE"
-    mkdir -p "$SNAPSHOTS/$REPONAME/$NAME"
-    cp -PR binaries/* "$SNAPSHOTSD/$REPONAME/$NAME/$DATE"
-    here=`pwd`
-    create_img $SNAPSHOTSD/$REPONAME/$NAME/$DATE
-    cd $here
-    clean_old_data $SNAPSHOTSD/$REPONAME/$NAME
-    cd $here
-    rm -f $SNAPSHOTS/$REPONAME/$NAME/latest
-    clean_old_data $SNAPSHOTS/$REPONAME/$NAME
-    cd $here
-    ln -sf $DATE "$SNAPSHOTS/$REPONAME/$NAME/latest"
-    ln -sf ../../data/$REPONAME/$NAME/$DATE $SNAPSHOTS/$REPONAME/$NAME/$DATE
-# delete all *-dbg_* packages
-    find $SNAPSHOTS/data/openbricks/geexbox-xbmc-*/* -name *-dbg_* -delete
-    make quickclean
-    log "Archiving $NAME log"
-    lbzip2 -9 $BUILDLOG
+    log "$CONFNAME clean failed"
   else
-    log "Archiving $NAME log"
-    echo "Build failed : local revision is $local_rev" >> $BUILDLOG 2>&1
-    lbzip2 -9 $BUILDLOG
-    log "$NAME build failed"
-    mailfail build
-    rm -f "$STAMPS/$REPONAME/$NAME"
+    log "Making $CONFNAME"
+    make >> $BUILDLOG 2>&1
+    if [ $? -ne 0 ]; then
+      log "$CONFNAME build failed"
+    else
+      log "$CONFNAME build successful"
+      mkdir -p "$SNAPSHOTS/$REPONAME/$CONFNAME/$DATE"
+      cp -PR *.html *.pdf images "$SNAPSHOTS/$REPONAME/$CONFNAME/$DATE/"
+      #cp -P $BASE/src/docs/*.png "$SNAPSHOTS/$REPONAME/$CONFNAME/$DATE/images/"
+
+      rm -f "$SNAPSHOTS/$REPONAME/$CONFNAME/latest"            
+      clean_old_data "$SNAPSHOTS/$REPONAME/$CONFNAME"
+
+      ln -sf $DATE "$SNAPSHOTS/$REPONAME/$CONFNAME/latest"
+    fi
+  fi
+fi
+
+
+# build active configurations
+for c in $ACTIVE_CONFIGS; do
+  cd $BUILD
+  prepare_to_build "$c" "$REPO/config/defconfigs/$c.conf"
+
+  if [ -e $CONFNAME/NEED_REBUILD ]; then
+    cd $CONFNAME
+    
+    log "Configuring $CONFNAME"
+    ./scripts/kconfiginit >> $BUILDLOG 2>&1
+    if grep -q 'CONFIG_OPT_TARGET_FLAT=y' $CONFFILE; then
+      sed \
+        -e 's:CONFIG_OPT_TARGET_FLAT=y:# CONFIG_OPT_TARGET_FLAT is not set:' \
+        -e 's:# CONFIG_OPT_TARGET_TARBALL is not set:CONFIG_OPT_TARGET_TARBALL=y:' \
+        -e 's:CONFIG_OPT_CONCURRENCY_MAKE_LEVEL=[0|9]:CONFIG_OPT_CONCURRENCY_MAKE_LEVEL=8:' \
+        < $CONFFILE > `ls -d build/build.host/kconfig-frontends-*`/.config
+    else
+      cp -P $CONFFILE `ls -d build/build.host/kconfig-frontends-*`/.config
+    fi
+    
+    make silentoldconfig >> $BUILDLOG 2>&1 || true
+
+    log "Cleaning $CONFNAME"
+    make quickclean >> $BUILDLOG 2>&1
+    if [ $? -ne 0 ]; then
+      log "$CONFNAME quickclean failed"
+      mailfail clean
+      rm -f "$STAMPS/$REPONAME/$CONFNAME"
+      continue
+    fi
+    
+    log "Making $CONFNAME"
+    rm -rf binaries
+    local_rev=`git log -1 --pretty="%h"`    
+
+    make >> $BUILDLOG 2>&1
+    if [ $? -ne 0 ]; then
+      log "Archiving $CONFNAME log"
+      echo "Build failed : local revision is $local_rev" >> $BUILDLOG 2>&1
+      compress $BUILDLOG
+      log "$CONFNAME build failed"
+      mailfail build
+      rm -f "$STAMPS/$REPONAME/$CONFNAME"
+      continue
+    fi
+
+    echo "Build successful : local revision is $local_rev" >> $BUILDLOG 2>&1
+    log "$CONFNAME build successful"
+    echo $DATE > "$STAMPS/$REPONAME/$CONFNAME"
+    mkdir -p "$SNAPSHOTSD/$REPONAME/$CONFNAME/$DATE"
+    mkdir -p "$SNAPSHOTS/$REPONAME/$CONFNAME"
+    cp -PR binaries/* "$SNAPSHOTSD/$REPONAME/$CONFNAME/$DATE"
+    # delete debug packages
+    find "$SNAPSHOTSD/$REPONAME/$CONFNAME/$DATE" -name *-dbg_*.opk -delete
+    # create disk images
+    create_img $SNAPSHOTSD/$REPONAME/$CONFNAME/$DATE
+
+    # remove old snapshots
+    clean_old_data $SNAPSHOTSD/$REPONAME/$CONFNAME
+
+    # re-create links
+    rm -f $SNAPSHOTS/$REPONAME/$CONFNAME/*    
+    for d in $SNAPSHOTSD/$REPONAME/$CONFNAME; do
+      n=`basename $d`
+      ln -sf ../../data/$REPONAME/$CONFNAME/$n $SNAPSHOTS/$REPONAME/$CONFNAME/$n
+    done
+    ln -sf $DATE $SNAPSHOTS/$REPONAME/$CONFNAME/latest
+
+    log "Archiving $CONFNAME log"
+    compress $BUILDLOG
+    
+    make quickclean
   fi
 done
 
-rm -f $STAMPS/lock
-log "Quitting"
-cp $LOGFILE $LOGS/$REPONAME/$REPONAME.$DATE.log
-echo "" > $LOGFILE
-lbzip2 -9 $LOGS/$REPONAME/$REPONAME.$DATE.log
 
+# rotate log file
+log "Quitting"
+mv -f $LOGFILE $LOGS/$REPONAME/$REPONAME.$DATE.log
+compress $LOGS/$REPONAME/$REPONAME.$DATE.log
+
+rm -f $STAMPS/lock
+exit 0
