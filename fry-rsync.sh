64a3d2f4e9186ff2505b9fa4960785ea7dd08d0f
warped-rudi <r.ihle@s-t.de>
2015-07-26 14:39:45 +0200

    fry-rsync.sh: Move sending log files to separate function, send previous rsync error log

--- fry-rsync.sh
+++ fry-rsync.sh
@@ -18,17 +18,20 @@ BWLIMIT=100
 XFERLOG=/tmp/rlog
 BUILDLOG=$LOGS/rsynclogs
 
+DATE=`date +%Y%m%d`
 
 log() {
   NOW=`date "+%b %d %T"`
   echo "$NOW [$$] $1" >> $LOGFILE
 }
 
-sendsnapshot () {
+sendlogs () {
   rsync_args="-t --size-only --bwlimit=$BWLIMIT --archive --delete --log-file=$XFERLOG --partial $LOGS/$REPONAME/*.?z* $SYNCTARGET:/data/logs-buildbot"
   log "Rsyncing build logs: $rsync_args"
   rsync $rsync_args >> $BUILDLOG 2>&1
-  
+}
+
+sendsnapshot () {  
   rsync_args="-t --size-only --bwlimit=$BWLIMIT --archive --delete --log-file=$XFERLOG --partial $SNAPSHOTSD $SYNCTARGET:/data/snapshots/"
   log "Rsyncing snapshot data: $rsync_args"  
   rsync $rsync_args >> $BUILDLOG 2>&1
@@ -69,6 +72,12 @@ fi
 
 /bin/echo -n $$ > $STAMPS/lockrsync
 
+# Move old rsync log
+cat $BUILDLOG >> $LOGS/$REPONAME/rsync.$DATE.log
+rm -f $BUILDLOG
+xz -z < $LOGS/$REPONAME/rsync.$DATE.log > $LOGS/$REPONAME/1-rsync.$DATE.log.xz
+
+sendlogs
 sendsnapshot
 sendsnapshotlink
 
