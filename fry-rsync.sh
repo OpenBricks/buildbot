6795d5d04274c3eca43f52c3d1468f53eed030c6
warped-rudi <r.ihle@s-t.de>
2015-07-29 10:02:25 +0200

    fry-rsync.sh: More debugging

--- fry-rsync.sh
+++ fry-rsync.sh
@@ -78,6 +78,7 @@ fi
 cat $RSYNCLOG >> $LOGS/$REPONAME/rsync.$DATE.log
 cat $LOGFILE >> $LOGS/$REPONAME/rsync.$DATE.log
 ls -lisa /etc/cron* /run/lock >> $LOGS/$REPONAME/rsync.$DATE.log
+crontab -l >> rsync.$DATE.log
 rm -f $RSYNCLOG
 xz -z < $LOGS/$REPONAME/rsync.$DATE.log > $LOGS/$REPONAME/1-rsync.$DATE.log.xz
 
